07b3c3c792096af7dc685760f3c3543107f9a783
Adam Deligianis <adeligianis@gmail.com>
2012-02-28 11:40:06 -0500

    added ignored files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tickets::Application.config.secret_token = '19a4a626bea341eb0cc06b0b64af3a5d15b0336b9cc812f3c687a3cc856b98d12dbf27263d35ac169a29d2227e0f3a68b5247679a0b237442bf4683472fd05aa'
